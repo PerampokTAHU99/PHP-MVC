b0011981ed0b616742251c1eab56c0e1ced488b0
“perampoktahu99” <theofahrizals@gmail.com>
2023-12-04 08:24:50 +0700

    wrapping db

--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -1,43 +1,19 @@
 <?php
-    class Mahasiswa_model{
-        private $dbh,$stmt; //database handler
+class Mahasiswa_model
+{
+    private $table = 'mahasiswa';
+    private $db;
 
-        public function __construct() {
-            $dsn = 'mysql:host=localhost;dbname=fundamental_php'; //data source name
-
-            try{
-                $this->dbh = new PDO($dsn, 'root', '');
-            }catch(PDOException $e){
-                die($e->getMessage());
-            }
-        }
-
-        // private $mhs = [
-        //         [
-        //             "name" => "Theo Fahrizal Syam",
-        //             "nim" => "5190411052",
-        //             "email" => "theof@gmail.com",
-        //             "prodi" => "Teknik Informatika",
-        //         ],
-        //         [
-        //             "name" => "Djarot Subarkah",
-        //             "nim" => "5190412053",
-        //             "email" => "jarot@gmail.com",
-        //             "prodi" => "Teknik Nuklir",
-        //         ],
-        //         [
-        //             "name" => "Diane Mariah",
-        //             "nim" => "519031123",
-        //             "email" => "Diane@gmail.com",
-        //             "prodi" => "Psikologi",
-        //         ]
-                
-        //     ];
-            public function getAllMahasiswa(){
-                $this->stmt = $this->dbh->prepare('SELECT * FROM mahasiswa');
-                $this->stmt->execute();
-                return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
-            }
+    public function __construct()
+    {
+        $this->db = new Database;
     }
+
+    public function getAllMahasiswa()
+    {
+        $this->db->query('SELECT * FROM ' . $this->table);
+        return $this->db->resultSet();
+    }
+}
 
 ?>
--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -4,13 +4,13 @@
             <h3>Daftar Mahasiswa</h3>
             <?php foreach ($data['mhs'] as $mhs): ?>
                 <ul>
-                    <li><?=$mhs['name']?></li>
-                    <li><?=$mhs['nim']?></li>
-                    <li><?=$mhs['email']?></li>
-                    <li><?=$mhs['programStudy']?></li>
-                    <li><img src="<?=$mhs['picture']?>" alt="picture" width="200"></li>
+                    <li><?=$mhs['name'];?></li>
+                    <li><?=$mhs['nim'];?></li>
+                    <li><?=$mhs['email'];?></li>
+                    <li><?=$mhs['programStudy'];?></li>
+                    <li><img src="img/<?=$mhs['picture'];?>" alt="picture" width="200"></li>
                 </ul>
             <?php endforeach; ?>
         </div>
     </div>
-</div>
+</div>  
